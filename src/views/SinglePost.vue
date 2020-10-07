020d700d66a17e69b49bb856b70c47922f46dcf8
Abdulqudduus Babalola <babalolajnr@gmail.com>
2020-10-07 11:22:34 +0100

    ➕ added posts tag in single post page

--- src/views/SinglePost.vue
+++ src/views/SinglePost.vue
@@ -134,21 +134,14 @@
                 </div>
                 <p v-html="post.body"></p>
               </div>
-              <p>Quibusdam autem, quas molestias recusandae aperiam molestiae modi qui ipsam vel. Placeat tenetur veritatis tempore quos impedit dicta, error autem, quae sint inventore ipsa quidem. Quo voluptate quisquam reiciendis, minus, animi minima eum officia doloremque repellat eos, odio doloribus cum.</p>
-              <p>Temporibus quo dolore veritatis doloribus delectus dolores perspiciatis recusandae ducimus, nisi quod, incidunt ut quaerat, magnam cupiditate. Aut, laboriosam magnam, nobis dolore fugiat impedit necessitatibus nisi cupiditate, quas repellat itaque molestias sit libero voluptas eveniet omnis illo ullam dolorem minima.</p>
-              <p>Porro amet accusantium libero fugit totam, deserunt ipsa, dolorem, vero expedita illo similique saepe nisi deleniti. Cumque, laboriosam, porro! Facilis voluptatem sequi nulla quidem, provident eius quos pariatur maxime sapiente illo nostrum quibusdam aliquid fugiat! Earum quod fuga id officia.</p>
-              <p>Illo magnam at dolore ad enim fugiat ut maxime facilis autem, nulla cumque quis commodi eos nisi unde soluta, ipsa eius aspernatur sint atque! Nihil, eveniet illo ea, mollitia fuga accusamus dolor dolorem perspiciatis rerum hic, consectetur error rem aspernatur!</p>
-
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus magni explicabo id molestiae, minima quas assumenda consectetur, nobis neque rem, incidunt quam tempore perferendis provident obcaecati sapiente, animi vel expedita omnis quae ipsa! Obcaecati eligendi sed odio labore vero reiciendis facere accusamus molestias eaque impedit, consequuntur quae fuga vitae fugit?</p>
-            </div>
+             </div>
 
             <div class="pt-5">
               <p>
-                Categories:
-                <a href="#">Food</a>,
-                <a href="#">Travel</a> Tags:
-                <a href="#">#manila</a>,
-                <a href="#">#asia</a>
+                Category:
+                <a href="#">{{ post.category.name }}</a>
+                 Tags:
+                <a v-for="tag in post.tags" :key="tag" href="#">{{ tag }}</a>
               </p>
             </div>
 
